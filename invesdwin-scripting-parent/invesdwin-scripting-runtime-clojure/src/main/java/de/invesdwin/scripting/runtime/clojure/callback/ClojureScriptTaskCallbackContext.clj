3ac04785c473236c702f3ca402e8cbe4a514268d
Edwin Stang <gsubes@gmail.com>
2024-10-21 23:42:19 +0200

    fix clojure

--- invesdwin-scripting-parent/invesdwin-scripting-runtime-clojure/src/main/java/de/invesdwin/scripting/runtime/clojure/callback/ClojureScriptTaskCallbackContext.clj
+++ invesdwin-scripting-parent/invesdwin-scripting-runtime-clojure/src/main/java/de/invesdwin/scripting/runtime/clojure/callback/ClojureScriptTaskCallbackContext.clj
@@ -2,7 +2,7 @@
 	(if-not (. binding containsKey "clojureScriptTaskCallbackContextUuid") 
         (throw (Exception. "IScriptTaskCallback not available"))
     )
-    (let [^de.invesdwin.scripting.runtime.clojure.callback.ClojureScriptTaskCallbackContext context (. de.invesdwin.context.clojure.callback.ClojureScriptTaskCallbackContext (getContext clojureScriptTaskCallbackContextUuid))]
+    (let [^de.invesdwin.scripting.runtime.clojure.callback.ClojureScriptTaskCallbackContext context (. de.invesdwin.scripting.runtime.clojure.callback.ClojureScriptTaskCallbackContext (getContext clojureScriptTaskCallbackContextUuid))]
 	    (. context invoke methodName (into-array Object parameters))
     )
 )
