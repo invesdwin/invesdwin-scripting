abcaa006848fbec52ed8e09a1260a9703a5a4183
Edwin Stang <gsubes@gmail.com>
2025-01-26 21:32:06 +0100

    unwrapping eval now working

--- invesdwin-scripting-parent/invesdwin-scripting-haskell/invesdwin-scripting-haskell-runtime-ghci/src/main/java/de/invesdwin/scripting/haskell/runtime/ghci/callback/file/FileScriptTaskCallbackContext.hs
+++ invesdwin-scripting-parent/invesdwin-scripting-haskell/invesdwin-scripting-haskell-runtime-ghci/src/main/java/de/invesdwin/scripting/haskell/runtime/ghci/callback/file/FileScriptTaskCallbackContext.hs
@@ -7,14 +7,14 @@ import Control.Concurrent(threadDelay)
 import System.Directory(renameFile)
 import Data.Typeable (Typeable)
 
--- :{
+:{
 -- https://stackoverflow.com/questions/57633136/how-to-write-a-retryforever-function-in-haskell-using-exception-handling
---retryForever :: IO a -> IO a
---retryForever prog = catchAll prog retry
---    where retry ex = do
---        threadDelay 1000
---        retryForever prog
--- :}
+retryForever :: IO a -> IO a
+retryForever prog = catchAll prog retry
+    where retry ex = do
+        threadDelay 1000
+        retryForever prog
+:}
 
 -- eval @Int "1 + 1"
 :{
@@ -28,16 +28,15 @@ eval s = do
            Right y -> return y
 :}
 
--- :{
---callback :: (Show args) => [args] -> IO result
---callback args = do
---    let message = show ( toJSON ( args ) )
---    writeFile scriptTaskCallbackContextRequestPartFile message
---    renameFile scriptTaskCallbackContextRequestPartFile scriptTaskCallbackContextRequestFile
---    returnExpression <- readFile scriptTaskCallbackContextResponseFile
---    Right evaluated <- eval returnExpression
---    evaluated
---    where scriptTaskCallbackContextRequestPartFile = {SCRIPT_TASK_CALLBACK_CONTEXT_REQUEST_PART_FILE}
---          scriptTaskCallbackContextRequestFile = {SCRIPT_TASK_CALLBACK_CONTEXT_REQUEST_FILE}
---          scriptTaskCallbackContextResponseFile = {SCRIPT_TASK_CALLBACK_CONTEXT_RESPONSE_FILE}
--- :}
+:{
+callback :: (Show args) => [args] -> IO result
+callback args = do
+    let message = show ( toJSON ( args ) )
+    writeFile scriptTaskCallbackContextRequestPartFile message
+    renameFile scriptTaskCallbackContextRequestPartFile scriptTaskCallbackContextRequestFile
+    returnExpression <- readFile scriptTaskCallbackContextResponseFile
+    eval returnExpression
+    where scriptTaskCallbackContextRequestPartFile = {SCRIPT_TASK_CALLBACK_CONTEXT_REQUEST_PART_FILE}
+          scriptTaskCallbackContextRequestFile = {SCRIPT_TASK_CALLBACK_CONTEXT_REQUEST_FILE}
+          scriptTaskCallbackContextResponseFile = {SCRIPT_TASK_CALLBACK_CONTEXT_RESPONSE_FILE}
+:}
