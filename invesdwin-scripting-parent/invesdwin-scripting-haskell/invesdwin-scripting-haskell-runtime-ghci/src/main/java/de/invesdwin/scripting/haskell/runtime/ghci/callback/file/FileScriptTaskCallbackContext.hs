4e1f8bab510691802effc38d14cd5c93c1c249ba
Edwin Stang <gsubes@gmail.com>
2025-01-26 21:27:49 +0100

    unwrapping eval now working

--- invesdwin-scripting-parent/invesdwin-scripting-haskell/invesdwin-scripting-haskell-runtime-ghci/src/main/java/de/invesdwin/scripting/haskell/runtime/ghci/callback/file/FileScriptTaskCallbackContext.hs
+++ invesdwin-scripting-parent/invesdwin-scripting-haskell/invesdwin-scripting-haskell-runtime-ghci/src/main/java/de/invesdwin/scripting/haskell/runtime/ghci/callback/file/FileScriptTaskCallbackContext.hs
@@ -1,39 +1,49 @@
 -- cabal update && cabal install aeson OR pacman -S haskell-aseon
 import Data.Aeson
 -- cabal update && cabal install hint OR pacman -S haskell-hint
-import qualified Language.Haskell.Interpreter as Hint
+import Language.Haskell.Interpreter
 import Control.Monad.Catch(catchAll)
 import Control.Concurrent(threadDelay)
 import System.Directory(renameFile)
 import Data.Typeable (Typeable)
 
-:{
+-- :{
 -- https://stackoverflow.com/questions/57633136/how-to-write-a-retryforever-function-in-haskell-using-exception-handling
-retryForever :: IO a -> IO a
-retryForever prog = catchAll prog retry
-    where retry ex = do
-        threadDelay 1000
-        retryForever prog
+--retryForever :: IO a -> IO a
+--retryForever prog = catchAll prog retry
+--    where retry ex = do
+--        threadDelay 1000
+--        retryForever prog
+-- :}
+
+:{
+evalEither :: forall t. Typeable t
+     => String -> IO (Either InterpreterError t)
+evalEither s = runInterpreter $ do
+  setImports ["Prelude"]
+  interpret s (as :: t)
 :}
 
+-- eval @Int "1 + 1"
 :{
 eval :: forall t. Typeable t
-     => String -> IO (Either Hint.InterpreterError t)
-eval s = Hint.runInterpreter $ do
-  Hint.setImports ["Prelude"]
-  Hint.interpret s (Hint.as :: t)
+     => String -> IO (t)
+eval s = do 
+      x <- evalEither @t s
+      case x of
+         Right y -> return y
 :}
 
-:{
-callback :: (Show args) => [args] -> IO result
-callback args = do
-    let message = show ( toJSON ( args ) )
-    writeFile scriptTaskCallbackContextRequestPartFile message
-    renameFile scriptTaskCallbackContextRequestPartFile scriptTaskCallbackContextRequestFile
-    returnExpression <- readFile scriptTaskCallbackContextResponseFile
-    Right evaluated <- eval returnExpression
-    evaluated
-    where scriptTaskCallbackContextRequestPartFile = {SCRIPT_TASK_CALLBACK_CONTEXT_REQUEST_PART_FILE}
-          scriptTaskCallbackContextRequestFile = {SCRIPT_TASK_CALLBACK_CONTEXT_REQUEST_FILE}
-          scriptTaskCallbackContextResponseFile = {SCRIPT_TASK_CALLBACK_CONTEXT_RESPONSE_FILE}
-:}
+-- :{
+--callback :: (Show args) => [args] -> IO result
+--callback args = do
+--    let message = show ( toJSON ( args ) )
+--    writeFile scriptTaskCallbackContextRequestPartFile message
+--    renameFile scriptTaskCallbackContextRequestPartFile scriptTaskCallbackContextRequestFile
+--    returnExpression <- readFile scriptTaskCallbackContextResponseFile
+--    Right evaluated <- eval returnExpression
+--    evaluated
+--    where scriptTaskCallbackContextRequestPartFile = {SCRIPT_TASK_CALLBACK_CONTEXT_REQUEST_PART_FILE}
+--          scriptTaskCallbackContextRequestFile = {SCRIPT_TASK_CALLBACK_CONTEXT_REQUEST_FILE}
+--          scriptTaskCallbackContextResponseFile = {SCRIPT_TASK_CALLBACK_CONTEXT_RESPONSE_FILE}
+-- :}
