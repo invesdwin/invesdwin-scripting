4d53bb6d514b4de0f3430eaa0b20747c0a97d1f6
Edwin Stang <gsubes@gmail.com>
2025-07-20 16:25:48 +0200

    parallel test also working

--- invesdwin-scripting-parent/invesdwin-scripting-rust/invesdwin-scripting-rust-runtime-evcxr/src/test/java/de/invesdwin/scripting/rust/runtime/evcxr/EvcxrScriptTaskRunnerRustTest.java
+++ invesdwin-scripting-parent/invesdwin-scripting-rust/invesdwin-scripting-rust-runtime-evcxr/src/test/java/de/invesdwin/scripting/rust/runtime/evcxr/EvcxrScriptTaskRunnerRustTest.java
@@ -2,6 +2,7 @@ package de.invesdwin.scripting.rust.runtime.evcxr;
 
 import javax.annotation.concurrent.NotThreadSafe;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import de.invesdwin.context.test.ATest;
@@ -20,6 +21,7 @@ public class EvcxrScriptTaskRunnerRustTest extends ATest {
         new InputsAndResultsTests(runner).test();
     }
 
+    @Disabled("very slow, thus disabled per default")
     @Test
     public void testParallel() {
         new InputsAndResultsTests(runner).testParallel();
