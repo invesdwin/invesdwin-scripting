3f3a509c70f05affab8dadf80e9250b621d500f1
Edwin Stang <gsubes@gmail.com>
2025-08-18 21:13:43 +0200

    PER-960 fix tearDownOnce

--- invesdwin-scripting-parent/invesdwin-scripting-haskell/invesdwin-scripting-haskell-runtime-contract/src/main/java/de/invesdwin/scripting/haskell/runtime/contract/internal/ProvidedScriptTaskRunnerHaskellStub.java
+++ invesdwin-scripting-parent/invesdwin-scripting-haskell/invesdwin-scripting-haskell-runtime-contract/src/main/java/de/invesdwin/scripting/haskell/runtime/contract/internal/ProvidedScriptTaskRunnerHaskellStub.java
@@ -3,6 +3,7 @@ package de.invesdwin.scripting.haskell.runtime.contract.internal;
 import javax.annotation.concurrent.NotThreadSafe;
 
 import de.invesdwin.context.test.ATest;
+import de.invesdwin.context.test.TestContext;
 import de.invesdwin.context.test.stub.StubSupport;
 import de.invesdwin.scripting.haskell.runtime.contract.ProvidedScriptTaskRunnerHaskell;
 import jakarta.inject.Named;
@@ -12,7 +13,10 @@ import jakarta.inject.Named;
 public class ProvidedScriptTaskRunnerHaskellStub extends StubSupport {
 
     @Override
-    public void tearDownOnce(final ATest test) throws Exception {
+    public void tearDownOnce(final ATest test, final TestContext ctx) {
+        if (!ctx.isFinished()) {
+            return;
+        }
         ProvidedScriptTaskRunnerHaskell.setProvidedInstance(null);
     }
 
--- invesdwin-scripting-parent/invesdwin-scripting-rust/invesdwin-scripting-rust-runtime-contract/src/main/java/de/invesdwin/scripting/rust/runtime/contract/internal/ProvidedScriptTaskRunnerRustStub.java
+++ invesdwin-scripting-parent/invesdwin-scripting-rust/invesdwin-scripting-rust-runtime-contract/src/main/java/de/invesdwin/scripting/rust/runtime/contract/internal/ProvidedScriptTaskRunnerRustStub.java
@@ -3,6 +3,7 @@ package de.invesdwin.scripting.rust.runtime.contract.internal;
 import javax.annotation.concurrent.NotThreadSafe;
 
 import de.invesdwin.context.test.ATest;
+import de.invesdwin.context.test.TestContext;
 import de.invesdwin.context.test.stub.StubSupport;
 import de.invesdwin.scripting.rust.runtime.contract.ProvidedScriptTaskRunnerRust;
 import jakarta.inject.Named;
@@ -12,7 +13,10 @@ import jakarta.inject.Named;
 public class ProvidedScriptTaskRunnerRustStub extends StubSupport {
 
     @Override
-    public void tearDownOnce(final ATest test) throws Exception {
+    public void tearDownOnce(final ATest test, final TestContext ctx) {
+        if (!ctx.isFinished()) {
+            return;
+        }
         ProvidedScriptTaskRunnerRust.setProvidedInstance(null);
     }
 
