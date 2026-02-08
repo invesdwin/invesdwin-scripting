69666eb1e84ee5445e79660b909142c40af5306d
Edwin Stang <gsubes@gmail.com>
2026-02-08 15:48:44 +0100

    reduce number of functions

--- invesdwin-scripting-parent/invesdwin-scripting-rust/invesdwin-scripting-rust-runtime-contract/src/main/java/de/invesdwin/scripting/rust/runtime/contract/callback/file/FileScriptTaskCallbackContext.rs
+++ invesdwin-scripting-parent/invesdwin-scripting-rust/invesdwin-scripting-rust-runtime-contract/src/main/java/de/invesdwin/scripting/rust/runtime/contract/callback/file/FileScriptTaskCallbackContext.rs
@@ -5,31 +5,6 @@ use std::time::Duration;
 use rhai::{Engine, Scope, Dynamic};
 use serde_json;
 
-fn wait_for_file(file_path: &str) {
-    loop {
-        if fs::metadata(file_path).is_ok() {
-            return;
-        }
-        thread::sleep(Duration::from_millis(1));
-    }
-}
-
-fn write_request(method: &str, params: &[serde_json::Value]) -> String {
-    let mut json_array = serde_json::json!([method]);
-    if let Some(array) = json_array.as_array_mut() {
-        for param in params {
-            array.push(param.clone());
-        }
-    }
-    json_array.to_string()
-}
-
-fn evaluate_response(expression: &str) -> Dynamic {
-    let engine = Engine::new();
-    let mut scope = Scope::new();
-    engine.eval_with_scope::<Dynamic>(&mut scope, expression).unwrap()
-}
-
 pub fn param<T: serde::Serialize>(value: T) -> serde_json::Value {
     serde_json::to_value(value).unwrap()
 }
@@ -38,11 +13,27 @@ pub fn callback(method_name: &str, parameters: &[serde_json::Value]) -> Dynamic 
 	let requestPartFile = {SCRIPT_TASK_CALLBACK_CONTEXT_REQUEST_PART_FILE};
 	let requestFile = {SCRIPT_TASK_CALLBACK_CONTEXT_REQUEST_FILE};
 	let responseFile = {SCRIPT_TASK_CALLBACK_CONTEXT_RESPONSE_FILE};
-    let request = write_request(method_name, parameters);
+	
+	//write request
+	let mut json_array = serde_json::json!([method_name]);
+	if let Some(array) = json_array.as_array_mut() {
+	    for param in parameters {
+	        array.push(param.clone());
+	    }
+	}
+	let request = json_array.to_string();
     fs::write(requestPartFile, &request).unwrap();
     fs::rename(requestPartFile, requestFile).unwrap();
-    wait_for_file(responseFile);
+	
+	//wait for response
+	while (!fs::metadata(responseFile).is_ok()) {
+	    thread::sleep(Duration::from_millis(1));
+	}
     let response = fs::read_to_string(responseFile).unwrap();
     fs::remove_file(responseFile).unwrap();
-    evaluate_response(&response)
+	
+	//evaluate response
+    let engine = Engine::new();
+    let mut scope = Scope::new();
+    engine.eval_with_scope::<Dynamic>(&mut scope, &response).unwrap()
 }
